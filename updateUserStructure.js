929b2c64ec3c90ce8e19e9c8667e4e6ed09d968a
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2021-01-07 14:13:25 +0300

    Create updateUserStructure.js

---
+++ updateUserStructure.js
@@ -0,0 +1,43 @@
+console.log("Starting updating the structure");
+console.log("Getting FS...");
+try{
+  fs = require("fs")
+}catch{
+  console.log("Error while getting the users.")
+  throw err;
+}
+console.log("Getting users.json...");
+try{
+  users = require("./users.json");
+}catch(err){
+   console.log("Error while getting the users.")
+   throw err;
+}
+console.log("Got users.json!");
+console.log("Setting Admin email array...");
+var adminEmail = ["admins@please.list"]
+console.log("Admin array set.");
+console.log("ForEach loop...");
+for (var user in users){
+  console.log("Updating the User structure of " + user);
+  console.log("Clearing Auth Keys for " + user);
+  users[user].keys = [];
+  console.log("Cleared Auth Keys for " + user);
+  console.log("Deleting 2FA for " + user);
+  delete users[user].twofa;
+  console.log("Deleted 2FA for " + user);
+
+  if (adminEmail.includes(user)){
+    console.log(user + " is Administrator, adding 2FA.");
+    users[user].twofa = true;
+  }
+}
+console.log("Local variable updated.");
+console.log("Writing variable to users.json...");
+fs.writeFile(__dirname + '/users.json', JSON.stringify(users), function(err) {
+  if (err) {
+    console.log("Error while writing the users.")
+    throw err;
+  }
+  console.log("Users updated successfully.")
+}); 
